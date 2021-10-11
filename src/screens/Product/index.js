2e87872cd5926a5f4e66fb8466c9144cf71e961d
Matheus Gomes <matheusgomes192@hotmail.com>
2021-10-11 10:32:23 -0300

    Criando arquivo Globals + Pasta screens + screen Product

---
+++ src/screens/Product/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from './product.module.scss';
+
+export default function Product () {
+    return(
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
