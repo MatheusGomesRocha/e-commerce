3e6c02ad8ff68635c31d9f85f06c316bc40bb514
Matheus Gomes <matheusgomes192@hotmail.com>
2021-10-12 07:42:59 -0300

    Criando Component Header e dimunuindo tamanho da Tela Product para caber no tamanho da tela

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import './globals.scss';
+
 import Product from './screens/Product';
+import Header from './components/Header';
 
 export default function App () {
   return(
     <div>
+      <Header />
+
       <Product />
     </div>
   )
