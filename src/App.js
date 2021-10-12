4f02acb78e9a465deeaab52b25f17a5b5f180141
Matheus Gomes <matheusgomes192@hotmail.com>
2021-10-12 09:38:32 -0300

    Criando Tela ProductList

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './globals.scss';
 
 import Product from './screens/Product';
 import Header from './components/Header';
+import ProductList from './screens/ProductList';
 
 export default function App () {
   return(
     <div>
       <Header />
 
-      <Product />
+      {/* <Product /> */}
+
+      <ProductList />
     </div>
   )
 }
---
+++ src/screens/ProductList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './productList.module.scss';
+
+export default function ProductList () {
+    return(
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
